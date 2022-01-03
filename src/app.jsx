8a892d2ef3c1075c309df33696fe331cf532566b
Nikhil Shisode <nikhilshisodemi6@gmail.com>
2022-01-03 17:48:07 +0530

    initial commit: setup

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>
+     Create React App Without CRA ☘️
+    </h1>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    hot: true,
+  },
+};
